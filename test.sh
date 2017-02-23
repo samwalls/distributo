1a2f196f93fc5b3381b15671ba4ebb07a2aa6615
samwalls <sam@s-w.io>
2017-02-23 07:15:35 +0000

    working test script

--- test.sh
+++ test.sh
@@ -2,6 +2,16 @@
 
 # Test Runner
 
+SHARE="/cs/scratch/${USER}/"
+
+#get the number of test times to capture
+if [ -z $1 ]; then
+    echo "expected number"
+    exit 1
+fi
+N_TEST=$1
+shift
+
 #get the list of clients to connect to
 if [ -z $1 ]; then
     echo "expected list of clients to test with"
@@ -25,6 +35,15 @@ fi
 
 TTY=$(tty)
 
+average() {
+    ELEMENTS=("$@")
+    SUM="0"
+    for item in "${ELEMENTS[@]}"; do
+        SUM=$(echo "x=${SUM} + ${item}; if(x<1) print 0; x" | bc)
+    done
+    AVG=$(echo "scale=10; x=${SUM} / ${#ELEMENTS[@]}; if(x<1) print 0; x" | bc)
+    echo "${AVG}"
+}
 #attempt the test over all files in measure/
 measureTime() {
     CMD="$1"
@@ -37,16 +56,106 @@ measureTime() {
     echo "${T_TOTAL}"
 }
 
-echo "===================TESTING========================"
-echo "Running tests for ${#CLIENTS[@]} clients:"
-for client in "${CLIENTS[@]}"; do
-    echo "- ${client}"
-done
-echo "=================================================="
+declare -A FILETIMES
+declare -A DSTTIMES
+declare -A SCPTIMES
+declare -A SFTPTIMES
 
-./cleanscratch.sh "${CLIENTS[@]}"
+#get a row of CSV output
+measureAverageTimeDst() {
+    N=$1
+    shift
+    FILE=$1
+    shift
+    ROW=()
+    SHARED_FILE="${WORKING_DIR}/measure/${FILE}"
+    COPIED_FILE="${SHARE}/${FILE}"
+
+    #measure for dst
+    echo "=========================================================="
+    echo "getting average for dst"
+    DATA=()
+    for i in `seq "${N}"`; do
+        ./cleanscratch.sh "${CLIENTS[@]}"
+        DATA+=( $(measureTime "./dst.sh ${SHARED_FILE} logs ${CLIENTS[@]}") )
+        ./verify.sh "${SHARED_FILE}" "${COPIED_FILE}" "${CLIENTS[@]}"
+    done
+    echo "averaging data: ${DATA[@]}"
+    AVG=$(average "${DATA[@]}")
+    DSTTIMES["${FILE}"]="${AVG}"
+}
+
+measureAverageTimeScp() {
+    N=$1
+    shift
+    FILE=$1
+    shift
+    ROW=()
+    SHARED_FILE="${WORKING_DIR}/measure/${FILE}"
+    COPIED_FILE="${SHARE}/${FILE}"
+
+    #measure for scp
+    echo "=========================================================="
+    echo "getting average for scp"
+    DATA=()
+    for i in `seq "${N}"`; do
+        ./cleanscratch.sh "${CLIENTS[@]}"
+        SUM_TIME="0"
+        for client in "${CLIENTS[@]}"; do
+            T=$(measureTime "scp ${SHARED_FILE} ${client}:${SHARE}/${FILE}")
+            SUM_TIME=$(echo "x=${SUM_TIME} + ${T}; if(x<1) print 0; x" | bc)
+        done
+        DATA+=( "${SUM_TIME}" )
+    done
+    ./verify.sh "${SHARED_FILE}" "${COPIED_FILE}" "${CLIENTS[@]}"
+    echo "averaging data: (${DATA[@]})"
+    AVG=$(average "${DATA[@]}")
+    SCPTIMES["${FILE}"]="${AVG}"
+}
+
+measureAverageTimeSftp() {
+    N=$1
+    shift
+    FILE=$1
+    shift
+    ROW=()
+    SHARED_FILE="${WORKING_DIR}/measure/${FILE}"
+    COPIED_FILE="${SHARE}/${FILE}"
+
+    #measure for sftp
+    echo "=========================================================="
+    echo "getting average for sftp"
+    DATA=()
+    for i in `seq "${N}"`; do
+        ./cleanscratch.sh "${CLIENTS[@]}"
+        SUM_TIME="0"
+        for client in "${CLIENTS[@]}"; do
+            T=$(measureTime "sftp ${client}:${SHARE} <<< $'put ${SHARED_FILE}'")
+            SUM_TIME=$(echo "x=${SUM_TIME} + ${T}; if(x<1) print 0; x" | bc)
+        done
+        DATA+=( "${SUM_TIME}" )
+    done
+    ./verify.sh "${SHARED_FILE}" "${COPIED_FILE}" "${CLIENTS[@]}"
+    echo "averaging data: ${DATA[@]}"
+    AVG=$(average "${DATA[@]}")
+    SFTPTIMES["${FILE}"]="${AVG}"
+}
+
 for file in $(ls measure); do
-    T=$(measureTime "./dst.sh measure/${file} logs ${CLIENTS[@]}")
-    echo "${T}"
+    measureAverageTimeDst "${N_TEST}" "${file}"
+    measureAverageTimeScp "${N_TEST}" "${file}"
+    measureAverageTimeSftp "${N_TEST}" "${file}"
 done
-./verify.sh "$(pwd)/measure/" "${CLIENTS[@]}"
+
+echo "====================RESULTS======================="
+echo "results in seconds, averaged over ${N_TEST} runs; for replication to ${#CLIENTS[@]} nodes"
+for file in $(ls measure); do
+    DST="${DSTTIMES[${file}]}"
+    SCP="${SCPTIMES[${file}]}"
+    SFTP="${SFTPTIMES[${file}]}"
+    echo "${file}: "
+    echo "DST : ${DST}"
+    echo "SCP : ${SCP}"
+    echo "SFTP: ${SFTP}"
+    echo ""
+done
--- verify.sh
+++ verify.sh
@@ -5,10 +5,17 @@ TTY=$(tty)
 
 #get the compare path
 if [ -z $1 ]; then
-    echo "expected path to compare share root with"
+    echo "expected file to compare"
     exit 1
 fi
-COMPARE="$1"
+LOCAL="$1"
+shift
+
+if [ -z $1 ]; then
+    echo "expected file to compare"
+    exit 1
+fi
+REMOTE="$1"
 shift
 
 #get the list of clients to connect to
@@ -24,7 +31,6 @@ while [ ! -z $1 ]; do
 done
 
 for client in "${CLIENTS[@]}"; do
-    echo "diffing for ${client}============================"
-    echo "comparing ${SHARE} to ${COMPARE}"
-    ssh -oStrictHostKeyChecking=no "${client}" "bash -c \"diff -rq ${SHARE} ${COMPARE}\"" > "${TTY}" 2> "${TTY}" < /dev/null
+    echo "comparing for ${client}: ${LOCAL} to ${REMOTE}"
+    ssh -oStrictHostKeyChecking=no "${client}" "bash -c \"diff -rq ${LOCAL} ${REMOTE}\"" > "${TTY}" 2> "${TTY}" < /dev/null
 done
